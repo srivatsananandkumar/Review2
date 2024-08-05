74e334ec7d3ed154ac9ce098c1d0c0442c1433d4
kinganonymousfreak <asrivatsan20043009@gmail.com>
2024-08-06 00:20:43 +0530

    mas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUI from './SignUp and SignIn/SignUI';
-
+/* import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+ */
 function App() {
   return (
     <Router>
